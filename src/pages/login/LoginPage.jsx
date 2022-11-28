857be6c884ff3caaa86ecfca853b66d153a309f9
Minji Kim <110662771+sloane323@users.noreply.github.com>
2022-11-28 15:11:24 +0900

    1128

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,21 +1,16 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import Input from '@mui/material/Input';
-import FilledInput from '@mui/material/FilledInput';
-import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
-import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { Button } from '@mui/material';
+
 
 
 const LoginPage = () => {
-
- 
 
   const [values, setValues] = React.useState({
     password: '',
@@ -38,29 +33,40 @@ const LoginPage = () => {
     event.preventDefault();
   };
 
+    const Loginbasicstyle = {
+      fontSize : '28px',
+      fontweight: "bold",
+      marginTop: '1em',
+    };
+  
+    const btnbasicstyle = {
+      backgroundColor :"#4a7eb2"
+        };
 
+    const btnbasicstyle2 = {
+      border :"3px soild pink"
+        };
+  
 
-  return ( <div>
+  return ( <div className='login-w' style={{textAlign:'center'}}>
 
 
     <div className="login">
-     <h1>로그인</h1>
-    <p> 서비스 시작을 위해 로그인을 해주세요 </p>
+     <h1 style={Loginbasicstyle}>로그인</h1>
+    <p style={{fontSize:'12px'}}> 서비스 시작을 위해 로그인을 해주세요 </p>
 <form> 
 
 
-<FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
+        <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
           <InputLabel htmlFor="standard-email">email</InputLabel>
           <Input
             id="standard-email"
             type="email"
-            value={values.email}
-          />
-         
+            value={values.email} />
         </FormControl>
 
   <br></br>
-<FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
+          <FormControl sx={{ m: 1, width: '25ch' }} variant="standard">
           <InputLabel htmlFor="standard-password">Password</InputLabel>
           <Input
             id="standard-password"
@@ -81,12 +87,14 @@ const LoginPage = () => {
           />
         </FormControl>
   <br></br>
-  <button> 아이디 찾기   </button>  / <button>비밀번호 찾기 </button>  <br></br>
+  <div style={{textAlign:'right'}}>
+  <Button style={{fontSize:'11px'}}> 아이디찾기</Button>  / 
+  <Button style={{fontSize:'11px'}} >비밀번호 찾기</Button>  <br></br>
+  </div>  <br></br>
 
-
-<button>로그인</button><br></br>
+  <Button variant="contained" style={btnbasicstyle}>로그인</Button><br></br>
 or<br></br>
-<button>google계정으로 계속하기 </button><br></br>
+<Button variant="outlined" style={btnbasicstyle2}>google 계정으로 계속하기</Button><br></br>
 
 </form>
 
