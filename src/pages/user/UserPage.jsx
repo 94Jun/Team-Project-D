0c0867222c00ef456b25276f7fdfbea100d5336b
KBJ97 <https://github.com/KBJ97/KBJ-git.git~>
2022-11-28 17:44:49 +0900

    project

--- src/pages/user/UserPage.jsx
+++ src/pages/user/UserPage.jsx
@@ -2,7 +2,13 @@ import Profile from "./profile";
 
 const UserPage = () => {
   return<div>
-    <Profile/> 이름
+    <Profile/>
+    <div>
+      <p>이름</p>
+      <p>소개</p>
+      <p>내용</p>
+    </div>
+    
   </div>;
   };
 
