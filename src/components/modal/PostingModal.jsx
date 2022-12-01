d6e1f834fe508f6f4f7d2921e1e4edfb3998324e
KimSoungMin1 <tjdals247470@naver.com>
2022-12-01 16:14:52 +0900

    1201

--- src/components/modal/PostingModal.jsx
+++ src/components/modal/PostingModal.jsx
@@ -3,39 +3,146 @@ import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
 import ClearIcon from "@mui/icons-material/Clear";
 import { useState } from "react";
 import Modal from "@mui/material/Modal";
-
+import AddCircleSharpIcon from "@mui/icons-material/AddCircleSharp";
+import { useSelector, useDispatch } from "react-redux";
+import { ADD_POSTING } from "../../modules/posting1";
+/* 버그...
+  이미지 추가 업로드시 같은 이미지 들어감 (다른 파일이랑 같이 업로드시)
+  미해결:setTagItem(e.target.value) 인풋 해쉬태그 text입력할때마다 리렌더 됨
+*/
+/* 기능 남은거
+  드래그앤 드롭
+  공개 비공개
+*/
 const PostingModal = (props) => {
-  const [data, setData] = useState([]);
-  const [imgFile, setImgFile] = useState(null);
-  const [imgs, setImgs] = useState("");
+  //업로드 이미지 저장 state
+  const [data, setData] = useState([]); //넘겨줄 저장소
+  const [imgs, setImgs] = useState();
+  //해쉬태그 저장 state
+  const [tagItem, setTagItem] = useState("");
+  const [tagList, setTagList] = useState([]); //넘겨줄 해쉬태그 저장
+  //text 저장공간 state 만들어야함
+  //reducer
+  const dispatch = useDispatch();
+
+  /*  {
+    pid: Math.random().toString(),
+    timestamp: new Date().toLocaleDateString(), //날짜 시간 초
+    contents: {
+      images: [], //업로드 이미지 배열로
+      hashtags: [],
+    }, //[""] 해쉬 태그 배열로
+  },*/
+  const addPosting = () => {
+    dispatch(
+      ADD_POSTING({
+        images: [data],
+        text: "가나다라",
+        hashtags: [tagList],
+      })
+    );
+  };
 
   //모달창 Open,Close 함수
   //const handleOpen = () => props.setOpen(true);
   const handleClose = () => props.setOpen(false);
 
+  // input 작성 하면 함수 발생
+  const valueOnChange = (e) => {
+    setTagItem(e.target.value);
+  };
+
+  //해쉬태그 데이터 저장함수
+  const submitTagItem = () => {
+    let updatedTagList = [...tagList];
+    updatedTagList.push(tagItem); //tagList []에  tagItem 값 을 넣어준다
+    setTagList(updatedTagList); //입력한 value를 tagList에 넣어줌
+    setTagItem(""); // 해쉬태그 값 입력후 인풋창 빈값으로 나타냄
+  };
+
+  //해쉬태그 조건 함수
+  const onKeyPress = (e) => {
+    //Enter key를 치면 함수 실행
+    if (e.target.value.length !== 0 && e.code === "Space") {
+      submitTagItem();
+    }
+  };
+
+  //해쉬태그 삭제 함수
+  const deleteTagItem = (e) => {
+    const deleteTagItem = e.target.parentElement.firstChild.innerText; //DOM에 접근
+    const filteredTagList = tagList.filter(
+      (tagItem) => tagItem !== deleteTagItem
+    );
+    setTagList(filteredTagList);
+  };
+
+  //해쉬태그 출력 map
+  const hashTag = tagList.map((tagItem, index) => {
+    return (
+      <div key={index} className={styles.hash_box}>
+        <span className={styles.tag}>{tagItem}</span>
+        <AddCircleSharpIcon
+          onClick={deleteTagItem}
+          className={styles.delfont}
+          fontSize={"small"}
+        />
+      </div>
+    );
+  });
   const handleChangeFile = (e) => {
-    //업로드 사진 미리보기 함수
-    setImgFile(e.target.files);
-    setData([]);
+    //업로드 데이터 담는 함수
+
     for (let i = 0; i < e.target.files.length; i++) {
+      // 추가 업로드 하면 e.target.files.length 값은 초기화
       if (e.target.files[i]) {
         let reader = new FileReader();
         reader.readAsDataURL(e.target.files[i]);
         reader.onloadend = () => {
           const resultImage = reader.result;
-          if (resultImage) {
-            const resultImageSub = resultImage.toString();
-            setData((data) => [...data, resultImageSub]);
-          }
+          setData((data) => [...data, resultImage]);
         };
       }
     }
   };
 
-  const clickImg = (id) => {
-    //미리보기 사진 클릭시 이미지 크게 보임 ==구현중../
-    setImgs(data.find((item) => item.length === id));
+  //대표이미지 함수
+  const clickImg = (index) => {
+    setImgs(data.find((imags) => imags.length === index));
   };
+
+  //삭제
+  const deleteImg = (index) => {
+    const imgNameArr = data.filter((idx) => idx !== index);
+    setData([...imgNameArr]);
+    // 이미지 삭세 대표이미지와 비교 후 같으면 같이 지워지게
+    if (imgs === index) {
+      setImgs("");
+    }
+  };
+
+  // 이미지 출력 mpa
+  const getPreviewImg = data.map((src) => {
+    return (
+      <li key={src}>
+        <AddCircleSharpIcon
+          onClick={() => deleteImg(src)}
+          className={`${styles.delfont} ${styles.delete_img}`}
+          fontSize={"small"}
+        />
+        <img
+          src={src}
+          alt=""
+          width={"50px"}
+          height={"50px"}
+          onClick={() => clickImg(src.length)}
+        />
+      </li>
+    );
+  });
+  //이미지 최대 4장 업로드 가능
+  const map =
+    data.length <= 4 ? getPreviewImg : `${alert("최대 4장")}${setData([])}`;
 
   return (
     <div>
@@ -62,7 +169,7 @@ const PostingModal = (props) => {
               <ClearIcon onClick={handleClose} />
             </p>
           </div>
-          <img src={imgs} alt="" width={"100%"} height={"460px"} />
+          <img src={imgs} alt="" width={"100%"} className={styles.image} />
           <div>
             <textarea
               name=""
@@ -71,21 +178,21 @@ const PostingModal = (props) => {
               rows="10"
               placeholder="내용을 작성해주세요"
             ></textarea>
+            <div className={styles.hash_box}>{hashTag}</div>
             <input
               type="text"
               placeholder="해시태그"
+              onChange={valueOnChange}
               className={styles.hash_input}
+              value={tagItem}
+              onKeyPress={onKeyPress}
             />
           </div>
+          <ul>{map}</ul>
           <div className={styles.bottom}>
-            <button className={styles.posting_btn}>추가</button>
-            <ul>
-              {data.map((src) => (
-                <li key={src.length} onClick={() => clickImg(src.id)}>
-                  <img src={src} alt="" width={"50px"} height={"50px"} />
-                </li>
-              ))}
-            </ul>
+            <button className={styles.posting_btn} onClick={addPosting}>
+              추가
+            </button>
           </div>
         </div>
       </Modal>
--- src/store1.js
+++ src/store1.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ExampleReducer from "./modules/example";
+import PostingReducer from "./modules/posting1";
 
 export default configureStore({
   reducer: {
     example: ExampleReducer,
+    posting: PostingReducer,
   },
 });
