3e7cfd439e4ee1c8f88b1cc87217990a14caff31
Minji Kim <110662771+sloane323@users.noreply.github.com>
2022-12-13 17:41:38 +0900

    make plan input

--- src/pages/plan/MakePlan.jsx
+++ src/pages/plan/MakePlan.jsx
@@ -10,7 +10,7 @@ const MakePlan = () => {
 
     return ( 
         <div>
-         <p> 즐거운 여행 계획을 시작하세요 </p>
+         <p> 즐거운 여행 계획을 시작하세요 </p><br /> 
 
          여행을 언제 시작하나요? {startDate} <br />
          <input type="date" id="start" name="trip-start"
@@ -23,20 +23,23 @@ const MakePlan = () => {
        min={startDate} max="2080-12-31" onChange={(e)=>setEndsDate(e.target.value)}  /><br />
         <button > 날짜? </button>
          0박 0일 <br />
-
-         여행을 어디로 떠나나요? <br /> 
+         <br /> 
+         여행을 어디로 떠나나요? <br /> <br /> 
          
-         <input type="text" /> <button>검색</button><br />        
+         <input type="text" /> <button>검색</button><br />        <br /> 
 
-         <h4>Day 1  </h4> 
+         <h4>Day 1  </h4> {startDate}
+         <input type="text" /> <button> 입력 </button>
          <li>부산역 </li>
          <li>도착시간 : 12:00 pm </li>
 
-         <h4>Day 3  </h4> 
+         <h4>Day 2  </h4>  
+         <input type="text" /> <button> 입력 </button>
          <li>부산역 </li>
          <li>도착시간 : 12:00 pm </li>
 
-         <h4>Day 4  </h4> 
+         <h4>Day 3  </h4> {endsDate}
+         <input type="text" /> <button> 입력 </button>
          <li>부산역 </li>
          <li>도착시간 : 12:00 pm </li>
 
