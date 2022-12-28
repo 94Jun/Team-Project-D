aebc46b2097d6852bb66ce5dd919607a44e89344
Byeongjun Park <pbj11241@gmail.com>
2022-12-28 17:41:06 +0900

    1228 여행 계획 수립 세부 내용 수정

--- src/components/Plan/PlanItem.jsx
+++ src/components/Plan/PlanItem.jsx
@@ -1,8 +1,10 @@
 import styles from "./PlanItem.module.css";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const PlanItem = (props) => {
   const navigate = useNavigate();
+  const currentUserInfo = useSelector(state => state.user.currentUserInfo);
   const gotoplan = () => {
     navigate(`/plan/${props.plan.planId}`);
   };
@@ -15,6 +17,7 @@ const PlanItem = (props) => {
           <p>여행시작 : {props.plan.startDate}</p>
           <p>여행끝 : {props.plan.endsDate}</p>
           <p>{props.plan.period}</p>
+          {currentUserInfo.uid !== props.plan.uid && <p>참여하는 여행</p>}
         </div>
       </button>
     </div>
--- src/components/Plan/PlanItemList.jsx
+++ src/components/Plan/PlanItemList.jsx
@@ -1,21 +1,66 @@
 import PlanItem from "./PlanItem";
 import { useEffect, useState } from "react";
-import { getqueryData } from "../../common";
+import { getNowValue, getqueryData } from "../../common";
 import { useSelector } from "react-redux";
+import { query, collection, where, getDocs } from "firebase/firestore";
+import { db } from "../../config/firebase";
 
 const PlanItemList = () => {
   const [planList, setPlanList] = useState();
-  const currentUserInfo = useSelector(state=>state.user.currentUserInfo)
+  const [participatedPlans, setParticipatedPlans] = useState();
+  const currentUserInfo = useSelector((state) => state.user.currentUserInfo);
+  const getparticipatedPlans = async () => {
+    const q = query(collection(db, "planList"), where("companion", "array-contains", currentUserInfo.uid), where("uid", "!=", currentUserInfo.uid));
+    const querySnapshot = await getDocs(q);
+    const loadedData = querySnapshot.docs.map((doc) => doc.data());
+    setParticipatedPlans(loadedData);
+  };
   useEffect(() => {
     getqueryData("planList", "uid", "==", currentUserInfo.uid, setPlanList);
-  }, [currentUserInfo])
-  
+    getparticipatedPlans();
+  }, [currentUserInfo]);
+  useEffect(() => {
+    if (participatedPlans) {
+      setPlanList((prev) => {
+        return [...prev, ...participatedPlans];
+      });
+    }
+  }, [participatedPlans]);
+  const prevPlanList =
+    planList && participatedPlans
+      ? planList.filter((plan) => {
+          const nowValue = getNowValue();
+          const endsValue = new Date(plan.endsDate.slice(0, 4), plan.endsDate.slice(5, 7) - 1, Number(plan.endsDate.slice(-2)) + 1).valueOf();
+          return nowValue > endsValue;
+        })
+      : null;
+  const nextPlanList =
+    planList && participatedPlans
+      ? planList.filter((plan) => {
+          const nowValue = getNowValue();
+          const endsValue = new Date(plan.endsDate.slice(0, 4), plan.endsDate.slice(5, 7) - 1, Number(plan.endsDate.slice(-2)) + 1).valueOf();
+          return nowValue <= endsValue;
+        })
+      : null;
   return (
     <div>
       <div>
-        {planList && planList.length !== 0 && planList.map((plan) => {
-          return <PlanItem key={plan.planId} plan={plan} />;
-        })}
+        <h3>지난 여행</h3>
+        {prevPlanList && prevPlanList.length !== 0 ? (
+          prevPlanList.map((plan) => {
+            return <PlanItem key={plan.planId} plan={plan} />;
+          })
+        ) : (
+          <div>완료한 여행이 없습니다.</div>
+        )}
+        <h3>진행(계획)중인 여행</h3>
+        {nextPlanList && nextPlanList.length !== 0 ? (
+          nextPlanList.map((plan) => {
+            return <PlanItem key={plan.planId} plan={plan} />;
+          })
+        ) : (
+          <div>계획중인 여행이 없습니다.</div>
+        )}
       </div>
     </div>
   );
